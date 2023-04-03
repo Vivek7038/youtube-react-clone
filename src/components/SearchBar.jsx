1deb9baab77ab83772268dad80d75b613234c417
Vivek7038 <vivek888chavan@gmail.com>
2023-04-03 20:03:07 +0530

    fixed video card

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,42 +1,43 @@
-import React, { useState } from 'react'
-import { Paper, IconButton } from '@mui/material'
-import Search from '@mui/icons-material/Search';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Paper, IconButton } from "@mui/material";
+import Search from "@mui/icons-material/Search";
+import { useNavigate } from "react-router-dom";
 const SearchBar = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
-   const  handleSubmit=(e)=>{
-  e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  if(searchTerm){
-    navigate(`/search/${searchTerm}`);
-    setSearchTerm('');
-  }
-   }
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm("");
+    }
+  };
 
   return (
     <Paper
-    component="form"
-    onSubmit={handleSubmit}
-    sx={{
-       borderRadius:20,
-       border:'1px solid #e3e3e3',
-       pl:2,
-       boxShadow:"none",
-       mr:{sm:5}
-    }}
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 1,
+        top: 1,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
     >
- <input 
-  className='search-bar'
-  placeholder='Search...'
-  value={searchTerm}
-  onChange={(e) => setSearchTerm(e.target.value)}
- />
- <IconButton type="submit" sx={{p:'10px',color:'red'}} >
-       <Search/>
- </IconButton>
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
     </Paper>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
